0d26d0bfef24063cc1b226498336dcf9ffc4545f
ozge <ozge.belenn@gmail.com>
2023-07-28 14:20:02 +0300

    adding useState

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ const messages = [
 ];
 
 const App = () => {
-  const step = 1;
+  //const step = 1;
+  const [step, setStep] = useState(1);
 
-  const handlePrevious = () => alert("Previous");
-  const handleNext = () => alert("Next");
+  const handlePrevious = () => {
+    step > 1 && setStep(step - 1);
+  };
+  const handleNext = () => {
+    step < 3 && setStep(step + 1);
+  };
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className={"message"}>
         Step {step}: {messages[step - 1]}
